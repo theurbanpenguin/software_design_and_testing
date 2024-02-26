ad4d94ec2f87ab3d2538fe6e29f0829d326a937e
theurbanpenguin <andrew.mallett@theurbanpenguin.com>
2024-02-26 15:02:37 +0000

    add simple tests

---
+++ testing/01/test_room_area.py
@@ -0,0 +1,14 @@
+import unittest
+from room_area import calculate_area
+
+
+class MyTestCase(unittest.TestCase):
+    def test_room_area(self):
+        self.assertEqual(calculate_area(5,4), 20)
+        self.assertEqual(calculate_area(3, 7), 21)
+        self.assertEqual(calculate_area(10, 10), 100)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
